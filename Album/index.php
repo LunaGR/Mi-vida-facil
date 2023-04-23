6fce92a3b75a891d33d7a3ec93da24fff1cb8367
Luna Gallardo Rendon <108183674+LunaGR@users.noreply.github.com>
2023-04-23 16:10:27 +0200

    Update index.php

--- Album/index.php
+++ Album/index.php
@@ -8,15 +8,21 @@ if (empty($_SESSION['usuario'])) {
 
 <html>
 <head>
-
+<link rel="stylesheet" type="text/css" href="foto.css">
 </head>
 <body>
-    <h1>Página del Usuario</h1>
-    <a href="crearalbum.php"><button class="btn success">Crear un album</button><br><br>
-    <a href="anadirfoto.php"><button class="btn success">Añadir una foto</button><br><br>
-    <a href="veralbum.php"><button class="btn success">Ver un album</button><br><br>
-    <a href="verperfil.php"><button class="btn success">Ver perfil</button><br><br>
-    <a href="buscarfoto.php"><button class="btn success">Buscar foto</button><br><br>
-    <a href="cerrarsesion.php"><button class="btn success">Cerrar sesion</button>
+    <div id="user">
+        <center>
+            <h1>Album de fotos</h1>
+            <a href="crearalbum.php"><button class="btn success">Crear un album</button><br><br>
+            <a href="anadirfoto.php"><button class="btn success">Añadir una foto</button><br><br>
+            <a href="veralbum.php"><button class="btn success">Ver un album</button><br><br>
+            <a href="verperfil.php"><button class="btn success">Ver perfil</button><br><br>
+            <a href="buscarfoto.php"><button class="btn success">Buscar foto</button><br><br>
+            <!-- <a href="cerrarsesion.php"><button class="btn success">Cerrar sesion</button> -->
+            <a href="http://localhost/mi-vida-facil/indexglobal.php"><input class="atras" type="button" value="Atras"> </a>
+        </center>
+    </div>
+
 </body>
 </html>
